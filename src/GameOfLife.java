a142f78941c8016096e1906f6e56635ec9455108
Eric Zhang <zhang.lyuan@gmail.com>
2013-07-12 23:47:15 +0800

    pass the case testNextGeneration

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -2,24 +2,26 @@
 public class GameOfLife {
     private int width;
     private int height;
+    private int size;
     private Cell [] cells;
 
     public GameOfLife(int w, int h){
         this.width = w;
         this.height = h;
+        this.size = w*h;
         this.__createCells();
     }
 
     public GameOfLife(int w, int h, String layout){
         this.width = w;
         this.height = h;
+        this.size = w*h;
         this.__createCells();
         this.__render(layout);
     }
 
     private void __render(String layout){
-        int size = this.width * this.height;
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < this.size; i++) {
             char c = layout.charAt(i);
             if (c == '1') {
                 this.cells[i].live();
@@ -28,9 +30,7 @@ public class GameOfLife {
     }
 
     private void __createCells(){
-        int size = this.width * this.height;
-
-        this.cells = new Cell[size];
+        this.cells = new Cell[this.size];
         int idx = 0;
         for (int x = 0; x < this.width; x++){
             for (int y = 0; y < this.height; y++){
@@ -42,18 +42,38 @@ public class GameOfLife {
         System.out.println();
     }
 
-    public void nextGeneration(){
+    private int aliveNeighboursCount(int col, int row){
+        return 0;
+    }
 
+    public void nextGeneration(){
+        int idx = 0;
+        for (int x = 0; x < this.width; x++){
+            for (int y = 0; y < this.height; y++){
+                idx = y * width + x;
+                int count = this.aliveNeighboursCount(x, y);
+                if (count == 3) {
+                    this.cells[idx].live();
+                    continue;
+                }
+
+                if (count < 2 || count > 4) {
+                    this.cells[idx].die();
+                }
+            }
+        }
     }
 
     public String currentGenerationLayout(){
         String layout = "";
+        for (int i = 0; i < this.size; i++){
+            if (this.cells[i].isAlive() == true){
+                layout += "1";
+            }
+            else{
+                layout += "0";
+            }
+        }
         return layout;
-    }
-
-    public Cell findCell(int col, int row){
-        int idx = 0;
-        idx = row * this.width + col;
-        return this.cells[idx];
     }
 }
