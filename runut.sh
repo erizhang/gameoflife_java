f5fe9817b5217a994f41991f3fdb442ada1d1818
Eric Zhang <zhang.lyuan@gmail.com>
2013-07-16 13:04:17 +0800

    update the travis ci config

--- runut.sh
+++ runut.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env sh
+#!/usr/bin/env ruby
+#!/usr/bin/env python
 export CLASSPATH=$CLASSPATH:./lib/junit-4.10.jar
 javac -sourcepath ./tst:./src ./tst/*.java
 java -classpath $CLASSPATH:./src:./tst TestRunner
